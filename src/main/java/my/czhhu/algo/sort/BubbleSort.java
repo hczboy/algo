200bdbf532fef02a1bafa165ad21f77da01613f8
arkie <arkie.hu@yahoo.com>
2018-05-23 01:28:15 +0800

    implement BubbleSort

---
+++ src/main/java/my/czhhu/algo/sort/BubbleSort.java
@@ -0,0 +1,36 @@
+package my.czhhu.algo.sort;
+
+public class BubbleSort extends AbstractSort {
+
+	public BubbleSort(int[] sa) {
+		super(sa);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void sort() {
+		if (a.length <= 1)
+			return;
+		int t;
+		for (int i = a.length - 1; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				if (a[j] > a[j + 1]) {
+					t = a[j];
+					a[j] = a[j + 1];
+					a[j + 1] = t;
+				}
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		int a[] = { 25, 1, 3, 7, 2 };
+		BubbleSort sort = new BubbleSort(a);
+		System.out.println(sort);
+		sort.sort();
+		System.out.println(sort);
+
+	}
+
+}
