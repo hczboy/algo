1b88d30b442ff275398471550b9bc886a3fe4e9a
czhhu <chengzhe.hu@polycom.com>
2018-05-28 17:17:21 +0800

    cocktail sort-a variant of bubble sort

--- src/main/java/my/czhhu/algo/sort/BubbleSort.java
+++ src/main/java/my/czhhu/algo/sort/BubbleSort.java
@@ -14,7 +14,7 @@ public class BubbleSort extends AbstractSort
     {
         if (a.length <= 1)
             return;
-        int t;
+
         boolean isSwap = false;
         for (int i = a.length - 1; i > 0; i--)
         {
@@ -32,12 +32,37 @@ public class BubbleSort extends AbstractSort
 
     }
 
+    public void cockTailSort()
+    {
+        if (a.length <= 1)
+            return;
+        int left = 0, right = a.length - 1;
+        while (left < right)
+        {
+            for (int i = left; i < right; i++)
+            {
+                if (a[i] > a[i + 1])
+                    swap(a, i, i + 1);
+            }
+            right--;
+            for (int j = right; j > left; j--)
+            {
+                if (a[j] < a[j - 1])
+                    swap(a, j, j - 1);
+            }
+            left++;
+
+        }
+
+    }
+
     public static void main(String[] args)
     {
         int a[] = { 25, 1, 3, 7, 2 };
         BubbleSort sort = new BubbleSort(a);
         System.out.println(sort);
-        sort.sort();
+        //sort.sort();
+        sort.cockTailSort();
         System.out.println(sort);
 
     }
