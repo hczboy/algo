460ec9357c8ebfc7c7bf1a00543492ab6b72d57c
czhhu <chengzhe.hu@polycom.com>
2018-05-29 10:33:38 +0800

    remove one reduntant loop

--- src/main/java/my/czhhu/algo/sort/ShellSort.java
+++ src/main/java/my/czhhu/algo/sort/ShellSort.java
@@ -1,44 +1,50 @@
 package my.czhhu.algo.sort;
 
-public class ShellSort extends AbstractSort {
+public class ShellSort extends AbstractSort
+{
 
-	public ShellSort(int[] sa) {
-		super(sa);
-		// TODO Auto-generated constructor stub
-	}
+    public ShellSort(int[] sa)
+    {
+        super(sa);
+        // TODO Auto-generated constructor stub
+    }
 
-	@Override
-	public void sort() {
-		if (a.length <= 1)
-			return;
-		int step = 1;
-		while (step * 3 < a.length)
-			step *= 3;
-		while (step >= 1) {
-			for (int i = 0; i < step; i++) {
-				if (i + step >= a.length)
-					break;
-				for (int j = i + step; j < a.length; j += step) {
-					int t = a[j];
-					while (j - step >= 0 && a[j - step] > t) {
-						a[j] = a[j - step];
-						j -= step;
-					}
-					a[j] = t;
-				}
-			}
-			step = step / 3;
-		}
-	}
+    @Override
+    public void sort()
+    {
+        if (a.length <= 1)
+            return;
+        int step = 1;
+        while (step * 3 < a.length)
+            step *= 3;
+        while (step >= 1)
+        {
+            for (int i = step; i < a.length; i++)
+            {
 
-	public static void main(String[] args) {
-		// int a[] = { 25, 1, 3, 7, 2, 5, 12, 5, 8, 6, 10, 4, 9, 13 };
-		int a[] = { 1, 3, 2 };
-		AbstractSort sort = new ShellSort(a);
-		System.out.println(sort);
-		sort.sort();
-		System.out.println(sort);
+                int t = a[i];
+                int j = i - step;
+                while (j >= 0 && a[j] > t)
+                {
+                    a[j + step] = a[j];
+                    j -= step;
+                }
+                a[j + step] = t;
 
-	}
+            }
+            step = step / 3;
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        int a[] = { 25, 1, 3, 7, 2, 5, 12, 5, 8, 6, 10, 4, 9, 13 };
+        //int a[] = { 1, 3, 2 };
+        AbstractSort sort = new ShellSort(a);
+        System.out.println(sort);
+        sort.sort();
+        System.out.println(sort);
+
+    }
 
 }
