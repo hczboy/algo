45f46968c18d4166286d48d0887b05757392e3ec
czhhu <chengzhe.hu@polycom.com>
2018-05-30 18:11:51 +0800

    create LIS

---
+++ src/main/java/my/czhhu/algo/dp/LIS.java
@@ -0,0 +1,21 @@
+package my.czhhu.algo.dp;
+
+/*
+ * 
+ * LIS: longest increasing subsequence
+ * */
+public class LIS
+{
+    public static int findLenOfLIS(int a[])
+    {
+
+        return 0;
+    }
+
+    public static void main(String[] args)
+    {
+        int a[] = { 5, 3, 4, 8, 6, 7 };
+
+    }
+
+}
