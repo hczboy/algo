8f402e6e6c3ce1a65d15761820b120be332c4697
czhhu <chengzhe.hu@polycom.com>
2018-05-31 18:17:54 +0800

    create GoldMine

---
+++ src/main/java/my/czhhu/algo/dp/GoldMine.java
@@ -0,0 +1,23 @@
+package my.czhhu.algo.dp;
+
+/*
+ * there are 100 workers and 5 gold mines
+ * http://www.cnblogs.com/sdjl/articles/1274312.html
+ * */
+
+public class GoldMine
+{
+    private static final int NbOfMines = 5;
+
+    private static final int TotalWorkers = 100;
+    private static final int[] workers = { 77, 22, 29, 50, 99 };// the workers for each gold mine;
+    private static final int[] golds = { 92, 22, 87, 46, 90 }; //the gold can be exploited for each gold mine;
+
+    public static void main(String[] args)
+    {
+        // TODO Implement main
+        throw new UnsupportedOperationException("main Not Implemented");
+
+    }
+
+}
