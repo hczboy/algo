5ec9b920dd624a508d77ef6b96f86885d722e34a
arkie <arkie.hu@yahoo.com>
2018-06-01 01:26:06 +0800

    create issue LongestPublicSubSeq

---
+++ src/main/java/my/czhhu/algo/dp/LongestPublicSubSeq.java
@@ -0,0 +1,14 @@
+package my.czhhu.algo.dp;
+/*
+ * the "fish" "fosh", the longestPublicSubSeq = 3
+ * 
+ * */
+
+public class LongestPublicSubSeq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
