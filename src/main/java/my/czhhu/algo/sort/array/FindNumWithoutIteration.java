0e89720e931e7f35228d18ef2d584722206fe56a
czhhu <chengzhe.hu@polycom.com>
2018-05-31 18:22:27 +0800

    create FindNumWithoutIteration

---
+++ src/main/java/my/czhhu/algo/sort/array/FindNumWithoutIteration.java
@@ -0,0 +1,19 @@
+package my.czhhu.algo.sort.array;
+
+/*
+ * there is a array, the abs of neighbors is 1, like {4,5,6,5,6,7,8,9,10,9}
+ * try to find the specified number n without iteration.
+ * 
+ * **/
+
+public class FindNumWithoutIteration
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Implement main
+        throw new UnsupportedOperationException("main Not Implemented");
+
+    }
+
+}
