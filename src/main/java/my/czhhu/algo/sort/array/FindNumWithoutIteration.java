dbad0649fbfdc33c85eb3bd1affe72e18bf1b496
czhhu <chengzhe.hu@polycom.com>
2018-06-01 11:31:22 +0800

    implement FindNumWithoutIteration

--- src/main/java/my/czhhu/algo/sort/array/FindNumWithoutIteration.java
+++ src/main/java/my/czhhu/algo/sort/array/FindNumWithoutIteration.java
@@ -8,12 +8,27 @@ package my.czhhu.algo.sort.array;
 
 public class FindNumWithoutIteration
 {
+    public static int find(int a[], int n)
+    {
+        int i = 0;
+        int j = 0;
+        while (i < a.length && (j = Math.abs(n - a[i])) >= 1)
+        {
+            i = i + j;
+        }
+        if (i >= a.length)
+            return -1;
+        if (n == a[i])
+            return i;
+        return -1;
+
+    }
 
     public static void main(String[] args)
     {
-        // TODO Implement main
-        throw new UnsupportedOperationException("main Not Implemented");
-
+        int a[] = { 4, 5, 6, 5, 6, 7, 8, 9, 10, 11 };
+        //int a[] = { 1, 2, 3, 4, 5, 6 };
+        System.out.println(find(a, 6));
     }
 
 }
