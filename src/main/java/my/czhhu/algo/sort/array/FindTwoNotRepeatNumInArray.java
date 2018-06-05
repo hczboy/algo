a2ccc5c5aa9597e476cce29c75e40d8fbf266878
czhhu <chengzhe.hu@polycom.com>
2018-06-05 18:43:20 +0800

    create FindTwoNotRepeatNumInArray

---
+++ src/main/java/my/czhhu/algo/sort/array/FindTwoNotRepeatNumInArray.java
@@ -0,0 +1,21 @@
+package my.czhhu.algo.sort.array;
+/*
+ *Give a[], only 2 elements occur 1 time, others are occur 2 times.
+ *find out the 2 elements occur only 1 time 
+ */
+
+public class FindTwoNotRepeatNumInArray
+{
+
+    public static int[] find(int a[])
+    {
+        return null;
+    }
+
+    public static void main(String[] args)
+    {
+        int a[] = { 1, 2, 3, 4, 1, 2, 3, 4, 0, 5 };
+
+    }
+
+}
