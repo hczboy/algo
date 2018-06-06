30e4509e2482e5edfdd9b27397ae344c261ec6ac
czhhu <chengzhe.hu@polycom.com>
2018-06-06 16:59:54 +0800

    TowersOfHanoi

---
+++ src/main/java/my/czhhu/algo/classic/TowersOfHanoi.java
@@ -0,0 +1,23 @@
+package my.czhhu.algo.classic;
+
+public class TowersOfHanoi
+{
+    static void run(int n, char frompeg, char topeg, char auxpeg)
+    {
+        if (n == 1)
+        {
+            System.out.println("move 1 disk from " + frompeg + " to " + topeg);
+            return;
+        }
+        run(n - 1, frompeg, auxpeg, topeg);
+        System.out.println("move " + n + " disk from " + frompeg + " to " + topeg);
+        run(n - 1, auxpeg, topeg, frompeg);
+    }
+
+    public static void main(String[] args)
+    {
+        run(3, '1', '2', '3');
+
+    }
+
+}
