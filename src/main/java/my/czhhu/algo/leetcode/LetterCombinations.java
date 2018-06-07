5c67eb0227fa71829f43c0aed2ddef9b49039630
czhhu <chengzhe.hu@polycom.com>
2018-06-07 18:45:42 +0800

    create LetterCombinations

---
+++ src/main/java/my/czhhu/algo/leetcode/LetterCombinations.java
@@ -0,0 +1,25 @@
+package my.czhhu.algo.leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * Given a digit string, return all possible letter combinations that the number could
+represent.
+A mapping of digit to letters (just like on the telephone buttons).
+ * */
+public class LetterCombinations
+{
+
+    private static List<String> res = new LinkedList<>();
+
+    private static final String dict[] = { "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+
+    public static void main(String[] args)
+    {
+        // TODO Implement main
+        throw new UnsupportedOperationException("main Not Implemented");
+
+    }
+
+}
