ca9afbf927209b159b091e08acd77be155aba337
arkie <arkie.hu@yahoo.com>
2018-06-08 23:22:59 +0800

    impl GenParentheses-recursive

--- src/main/java/my/czhhu/algo/leetcode/GenParentheses.java
+++ src/main/java/my/czhhu/algo/leetcode/GenParentheses.java
@@ -3,33 +3,31 @@ package my.czhhu.algo.leetcode;
 import java.util.LinkedList;
 import java.util.List;
 
-public class GenParentheses
-{
+public class GenParentheses {
 
-    public static List<String> gen(int n)
-    {
-        //Stack<String> stack = new Stack<>();
-        List<String> res = new LinkedList<>();
-        doGen(res, "", 0, n);
+	public static List<String> gen(int n) {
 
-    }
+		List<String> res = new LinkedList<>();
+		doGen(res, "", n, n);
+		return res;
 
-    private static void doGen(List<String> res, String prefix, int i, int n)
-    {
-        if (i > n)
-        {
-            res.add(prefix);
-            return;
-        }
-        doGen(res, prefix + "(", i + 1, n);
+	}
 
-    }
+	private static void doGen(List<String> res, String s, int left, int right) {
+		if (left == 0 && right == 0) {
+			res.add(s);
 
-    public static void main(String[] args)
-    {
-        // TODO Implement main
-        throw new UnsupportedOperationException("main Not Implemented");
+		}
+		if (left < 0 || right < 0 || left > right) // left >right, it means illegal case ")(" appears
+			return;
+		doGen(res, s + '(', left - 1, right);
+		doGen(res, s + ')', left, right - 1);
 
-    }
+	}
+
+	public static void main(String[] args) {
+		System.out.println(gen(3));
+
+	}
 
 }
