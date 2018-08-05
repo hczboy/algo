c1cd8230e3ab1591ce697fc8d17d2a3bda333d80
arkie <arkie.hu@yahoo.com>
2018-08-05 23:41:14 +0800

    make rld private

--- src/main/java/my/czhhu/algo/linklist/CommonOp.java
+++ src/main/java/my/czhhu/algo/linklist/CommonOp.java
@@ -2,43 +2,37 @@ package my.czhhu.algo.linklist;
 
 public class CommonOp {
 
-	static Node findMidNode(Node head)
-	{
-	    if (head == null)
-	    {
-	        return head;
-	    }
-	
-	    Node slow;
-	    Node fast = slow = head;
-	    while (fast.getNext() != null && fast.getNext().getNext() != null)
-	    {
-	        slow = fast.getNext();
-	        fast = fast.getNext().getNext();
-	    }
-	    return slow;
+	static Node findMidNode(Node head) {
+		if (head == null) {
+			return head;
+		}
+
+		Node slow;
+		Node fast = slow = head;
+		while (fast.getNext() != null && fast.getNext().getNext() != null) {
+			slow = fast.getNext();
+			fast = fast.getNext().getNext();
+		}
+		return slow;
 	}
 
-	static Node reverseLinkedList(Node head)
-	{
-	    if (head == null || head.getNext() == null)
-	        return head;
-	    return CommonOp.rld(head, head.getNext());
+	static Node reverseLinkedList(Node head) {
+		if (head == null || head.getNext() == null)
+			return head;
+		return CommonOp.rld(head, head.getNext());
 	}
 
-	static Node rld(Node cur, Node n)
-	{
-	    if (n.getNext() == null)
-	    {
-	        n.setNext(cur);
-	        return n;
-	    }
-	    Node tmp = n.getNext();
-	    n.setNext(cur);
-	    cur = n;
-	    n = tmp;
-	    return rld(cur, n);
-	
+	static private Node rld(Node cur, Node n) {
+		if (n.getNext() == null) {
+			n.setNext(cur);
+			return n;
+		}
+		Node tmp = n.getNext();
+		n.setNext(cur);
+		cur = n;
+		n = tmp;
+		return rld(cur, n);
+
 	}
 
 }
