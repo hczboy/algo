fce0e7b7fae746647588b8858b0f685e931a6974
arkie <arkie.hu@yahoo.com>
2018-08-09 01:17:49 +0800

    create two topic

---
+++ src/main/java/my/czhhu/algo/leetcode/Permutations46.java
@@ -0,0 +1,16 @@
+package my.czhhu.algo.leetcode;
+
+import java.util.List;
+
+//given a String "abc", output all premutations for this String 
+public class Permutations46 {
+
+	static List<String> permutations(String s) {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/my/czhhu/algo/lintcode/LRUCache134.java
@@ -0,0 +1,18 @@
+package my.czhhu.algo.lintcode;
+
+public class LRUCache134<K, V> {
+
+	private int capacity;
+
+	public V get(K key) {
+	}
+
+	public void set(K key, V value) {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
