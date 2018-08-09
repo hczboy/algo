204c0e4e6b637a5bade0dfd892693db5d5fc5ba9
czhhu <chengzhe.hu@polycom.com>
2018-08-09 10:40:02 +0800

    remove unnecessary variable count

--- src/main/java/my/czhhu/algo/leetcode/LongestSubstringKDistinct340.java
+++ src/main/java/my/czhhu/algo/leetcode/LongestSubstringKDistinct340.java
@@ -1,58 +1,69 @@
 package my.czhhu.algo.leetcode;
 
-public class LongestSubstringKDistinct340 {
+public class LongestSubstringKDistinct340
+{
 
-	static int solution(String s, int k) {
-		if (s == null || k < 1) {
-			return 0;
-		} // array c is a counter array to count number of each char in String s
-		int[] c = new int[256];
-		/*
-		 * the idea here is to maintain a slide window with most left index(i) and most
-		 * right index(j), the subString between i and j has no more than k distinct
-		 * chars go through each char, if first meet, dis++, count++ c[char]++ if dis>k,
-		 * move i to right else if NOT first meet, just j++, count++, c[char++], Key is
-		 * moving the slide window
-		 */
-		// dis: the distinct char number
-		// max: the max subString length of k distinct char
-		// count: one round of length of subString
-		//
-		int dis = 0, max = 0, count = 0, i = 0, j = 0;
-		while (j < s.length()) {
-			if (c[s.charAt(j)] == 0) {
-				dis++;
-				if (dis > k) {
-					max = Math.max(max, count);
-					while (dis > k) {
+    static int solution(String s, int k)
+    {
+        if (s == null || k < 1)
+        {
+            return 0;
+        } // array c is a counter array to count number of each char in String s
+        int[] c = new int[256];
+        /*
+         * the idea here is to maintain a slide window with most left index(i) and most
+         * right index(j), the subString between i and j has no more than k distinct
+         * chars go through each char, if first meet, dis++, count++ c[char]++ if dis>k,
+         * move i to right else if NOT first meet, just j++, count++, c[char++], Key is
+         * moving the slide window
+         */
+        // dis: the distinct char number
+        // max: the max subString length of k distinct char
+        // 
+        // count: one round of length of subString(removed, instead of computing by j-i+1)
+        // count = 0,
+        int dis = 0, max = 0, i = 0, j = 0;
+        while (j < s.length())
+        {
+            if (c[s.charAt(j)] == 0)
+            {
+                dis++;
+                if (dis > k)
+                {
+                    //max = Math.max(max, count);
+                    while (dis > k)
+                    {
 
-						c[s.charAt(i)] -= 1;
-						count--;
-						if (c[s.charAt(i)] == 0) {
-							dis--;
-						}
-						i++;
+                        c[s.charAt(i)] -= 1;
+                        //count--;
+                        if (c[s.charAt(i)] == 0)
+                        {
+                            dis--;
+                        }
+                        i++;
 
-					}
+                    }
 
-				}
-			}
-			c[s.charAt(j)] += 1;
+                }
+            }
+            c[s.charAt(j)] += 1;
 
-			count++;
-			j++;
+            //count++;
+            max = Math.max(max, j - i + 1);
+            j++;
 
-		}
-		return max;
-	}
+        }
+        return max;
+    }
 
-	public static void main(String[] args) {
-		String s = "eceba";
-		// String s = "ecebaaec";
-		// solution(s, 3) = 4; "eceb"
+    public static void main(String[] args)
+    {
+        //String s = "eceba";
+        String s = "ecebaaec";
+        // solution(s, 3) = 4; "eceb"
 
-		System.out.println(solution(s, 3));
+        System.out.println(solution(s, 3));
 
-	}
+    }
 
 }
