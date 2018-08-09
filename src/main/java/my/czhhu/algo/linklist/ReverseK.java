943bb40ca5286aac979cd2b3817b41b25655430c
czhhu <chengzhe.hu@polycom.com>
2018-08-09 18:55:04 +0800

    create reverseK

---
+++ src/main/java/my/czhhu/algo/linklist/ReverseK.java
@@ -0,0 +1,20 @@
+package my.czhhu.algo.linklist;
+
+import my.czhhu.algo.common.Node;
+
+public class ReverseK
+{
+
+    static Node ReverseK(Node head, int k)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        // TODO Implement main
+        throw new UnsupportedOperationException("main Not Implemented");
+
+    }
+
+}
