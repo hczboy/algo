fc84d28787c9b61e10508f0352943ca218ef1476
arkie <arkie.hu@yahoo.com>
2018-08-20 01:20:22 +0800

    another impl

--- src/main/java/my/czhhu/algo/leetcode/PermutationII47.java
+++ src/main/java/my/czhhu/algo/leetcode/PermutationII47.java
@@ -3,6 +3,7 @@ package my.czhhu.algo.leetcode;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -55,10 +56,58 @@ public class PermutationII47 {
 
 	}
 
+	public static List<String> permuU(String s) {
+		if (s == null || s.length() == 0)
+			return Collections.EMPTY_LIST;
+
+		List<String> res = new LinkedList<>();
+
+		permutation(s.toCharArray(), 0, res);
+		return res;
+
+	}
+
+	private static void permutation(char[] ca, int i, List<String> res) {
+		// TODO Auto-generated method stub
+		if (i == ca.length - 1) {
+			res.add(new String(ca));
+			return;
+		}
+
+		for (int j = i; j < ca.length; j++) {
+			if (!isAllowSwap(ca, i, j))
+				continue;
+			swap(ca, i, j);
+			permutation(ca, i + 1, res);
+			swap(ca, j, i);
+		}
+	}
+
+	private static boolean isAllowSwap(char[] ca, int start, int i) {
+		char c = ca[i];
+		for (int j = start; j < i; j++) {
+
+			if (c == ca[j])
+				return false;
+		}
+
+		return true;
+
+	}
+
+	private static void swap(char[] ca, int i, int j) {
+		if (i == j)
+			return;
+		char t = ca[i];
+		ca[i] = ca[j];
+		ca[j] = t;
+
+	}
+
 	public static void main(String[] args) {
 		String s = "112";
-
-		System.out.println(permu(s));
+		System.out.println(permuU(s));
+		// System.out.println(permu(s));
 	}
 
 }
