23b24ffeca839a3675570bcf36467c978886b2be
czhhu <chengzhe.hu@polycom.com>
2018-08-20 20:32:45 +0800

    reformat

--- src/main/java/my/czhhu/algo/leetcode/PermutationII47.java
+++ src/main/java/my/czhhu/algo/leetcode/PermutationII47.java
@@ -17,97 +17,115 @@ For example, [1,1,2] have the following unique permutations:
 [2,1,1]
 ]
  * */
-public class PermutationII47 {
+public class PermutationII47
+{
 
-	static List<String> permu(String s) {
-		if (s == null || s.length() == 0)
-			return Collections.EMPTY_LIST;
+    static List<String> permu(String s)
+    {
+        if (s == null || s.length() == 0)
+            return Collections.EMPTY_LIST;
 
-		Map<Character, Integer> map = new HashMap<>();
-		for (char c : s.toCharArray()) {
-			if (map.containsKey(c)) {
-				map.put(c, map.get(c).intValue() + 1);
-			} else {
-				map.put(c, Integer.valueOf(1));
-			}
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : s.toCharArray())
+        {
+            if (map.containsKey(c))
+            {
+                map.put(c, map.get(c).intValue() + 1);
+            }
+            else
+            {
+                map.put(c, Integer.valueOf(1));
+            }
 
-		}
-		List<String> res = new ArrayList<>();
+        }
+        List<String> res = new ArrayList<>();
 
-		permu(map, "", 0, res, s.length());
-		return res;
-	}
+        permu(map, "", 0, res, s.length());
+        return res;
+    }
 
-	private static void permu(Map<Character, Integer> map, String prefix, int start, List<String> res, int len) {
-		if (start == len) {
-			res.add(prefix);
-			return;
-		}
+    private static void permu(Map<Character, Integer> map, String prefix, int start, List<String> res, int len)
+    {
+        if (start == len)
+        {
+            res.add(prefix);
+            return;
+        }
 
-		for (Character c : map.keySet()) {
-			if (map.get(c) > 0) {
+        for (Character c : map.keySet())
+        {
+            if (map.get(c) > 0)
+            {
 
-				map.put(c, map.get(c).intValue() - 1);
-				permu(map, prefix + c, start + 1, res, len);
+                map.put(c, map.get(c).intValue() - 1);
+                permu(map, prefix + c, start + 1, res, len);
 
-				map.put(c, map.get(c).intValue() + 1);
-			}
-		}
+                map.put(c, map.get(c).intValue() + 1);
+            }
+        }
 
-	}
+    }
 
-	public static List<String> permuU(String s) {
-		if (s == null || s.length() == 0)
-			return Collections.EMPTY_LIST;
+    public static List<String> permuU(String s)
+    {
+        if (s == null || s.length() == 0)
+            return Collections.EMPTY_LIST;
 
-		List<String> res = new LinkedList<>();
+        List<String> res = new LinkedList<>();
 
-		permutation(s.toCharArray(), 0, res);
-		return res;
+        permutation(s.toCharArray(), 0, res);
+        return res;
 
-	}
+    }
 
-	private static void permutation(char[] ca, int i, List<String> res) {
-		// TODO Auto-generated method stub
-		if (i == ca.length - 1) {
-			res.add(new String(ca));
-			return;
-		}
+    private static void permutation(char[] ca, int i, List<String> res)
+    {
+        // TODO Auto-generated method stub
+        if (i == ca.length - 1)
+        {
+            res.add(new String(ca));
+            return;
+        }
 
-		for (int j = i; j < ca.length; j++) {
-			if (!isAllowSwap(ca, i, j))
-				continue;
-			swap(ca, i, j);
-			permutation(ca, i + 1, res);
-			swap(ca, j, i);
-		}
-	}
+        for (int j = i; j < ca.length; j++)
+        {
+            if (!isAllowSwap(ca, i, j))
+                continue;
+            swap(ca, i, j);
+            permutation(ca, i + 1, res);
+            swap(ca, j, i);
+        }
+    }
 
-	private static boolean isAllowSwap(char[] ca, int start, int i) {
-		char c = ca[i];
-		for (int j = start; j < i; j++) {
+    private static boolean isAllowSwap(char[] ca, int start, int i)
+    {
+        char c = ca[i];
+        for (int j = start; j < i; j++)
+        {
 
-			if (c == ca[j])
-				return false;
-		}
+            if (c == ca[j])
+                return false;
+        }
 
-		return true;
+        return true;
 
-	}
+    }
 
-	private static void swap(char[] ca, int i, int j) {
-		if (i == j)
-			return;
-		char t = ca[i];
-		ca[i] = ca[j];
-		ca[j] = t;
+    private static void swap(char[] ca, int i, int j)
+    {
+        if (i == j)
+            return;
+        char t = ca[i];
+        ca[i] = ca[j];
+        ca[j] = t;
 
-	}
+    }
 
-	public static void main(String[] args) {
-		String s = "112";
-		System.out.println(permuU(s));
-		// System.out.println(permu(s));
-	}
+    public static void main(String[] args)
+    {
+        String s = "1213";
+        System.out.println(permuU(s));
+        // System.out.println(permu(s));
+    }
 
 }
