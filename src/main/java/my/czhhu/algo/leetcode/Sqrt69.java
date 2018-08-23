935107a11c2f2c0e1c35702f08910dd94286e2ee
czhhu <chengzhe.hu@polycom.com>
2018-08-23 17:50:17 +0800

    Sqrt69

---
+++ src/main/java/my/czhhu/algo/leetcode/Sqrt69.java
@@ -0,0 +1,36 @@
+package my.czhhu.algo.leetcode;
+
+public class Sqrt69
+{
+
+    static int sqrt(int x)
+    {
+        if (x == 0 || x == 1)
+            return x;
+        int l = 0, r = x;
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+            if (x / mid == mid)
+                return mid;
+            if (x / mid < mid)
+            {
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+
+        }
+        return r;
+
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(sqrt(10));
+
+    }
+
+}
