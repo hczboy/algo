18eaf749a4d56bbe9702982b46127f4bce405a97
arkie <arkie.hu@yahoo.com>
2018-08-24 01:46:40 +0800

    editDistance72 create search2DMatrix74

--- src/main/java/my/czhhu/algo/leetcode/EditDistance72.java
+++ src/main/java/my/czhhu/algo/leetcode/EditDistance72.java
@@ -9,65 +9,57 @@ b) Delete a character
 c) Replace a character
  * 
  * */
-public class EditDistance72
-{
+public class EditDistance72 {
 
-    static int editDistance(String w1, String w2)
-    {
-        if (w1 == null && w2 == null)
-            return 0;
+	static int editDistance(String w1, String w2) {
+		if (w1 == null && w2 == null)
+			return 0;
 
-        if (w1 == null || w2 == null)
-        {
-            return w1 == null ? w2.length() : w1.length();
-        }
+		if (w1 == null || w2 == null) {
+			return w1 == null ? w2.length() : w1.length();
+		}
 
-        int w1l = w1.length();
-        int w2l = w2.length();
-        if (w1l == 0 || w2l == 0)
-        {
-            return w1l == 0 ? w2l : w1l;
-        }
+		int w1l = w1.length();
+		int w2l = w2.length();
+		if (w1l == 0 || w2l == 0) {
+			return w1l == 0 ? w2l : w1l;
+		}
 
-        int d[][] = new int[w1l][w2l];
-        if (w1.charAt(0) != w2.charAt(0))
-        {
-            d[0][0] = 1;
+		int d[][] = new int[w1l + 1][w2l + 1];
 
-        }
-        else
-        {
-            d[0][0] = 0;
+		// init if w1 is "", then all characters in w2 should be inserted
+		for (int j = 0; j <= w2l; j++) {
+			d[0][j] = j;
 
-        }
-        for (int j = 1; j < w2l; j++)
-        {
-            d[0][j] = d[0][j - 1] + 1;
+		}
 
-        }
+		for (int j = 0; j <= w1l; j++) {
+			d[j][0] = j;
 
-        for (int j = 1; j < w1l; j++)
-        {
-            d[j][0] = d[j - 1][0] + 1;
+		}
 
-        }
+		for (int i = 1; i <= w1l; i++) {
+			for (int j = 1; j <= w2l; j++) {
 
-        for (int i = 1; i < w1l; i++)
-        {
-            for (int j = 1; j < w2l; j++)
-            {
+				if (w1.charAt(i - 1) == w2.charAt(j - 1)) {
+					d[i][j] = d[i - 1][j - 1];
 
-            }
+				} else {
+					d[i][j] = Math.min(d[i][j - 1], Math.min(d[i - 1][j], d[i - 1][j - 1])) + 1;
+				}
+			}
 
-        }
+		}
 
-    }
+		return d[w1l][w2l];
 
-    public static void main(String[] args)
-    {
-        "cxii", "ixc"
-        throw new UnsupportedOperationException("main Not Implemented");
+	}
 
-    }
+	public static void main(String[] args) {
+
+		String w1 = "cxii", w2 = "ixci";
+		System.out.println(editDistance(w1, w2));
+
+	}
 
 }
---
+++ src/main/java/my/czhhu/algo/leetcode/Search2DMatrix74.java
@@ -0,0 +1,13 @@
+package my.czhhu.algo.leetcode;
+
+public class Search2DMatrix74 {
+
+	static boolean isExists(int[][] a, int target) {
+	}
+
+	public static void main(String[] args) {
+		int a[][] = { { 1, 3, 5, 7 }, { 10, 11, 16, 20 }, { 23, 30, 34, 50 } };
+
+	}
+
+}
