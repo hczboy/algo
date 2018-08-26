2751c65c824976a89c9d5a5e9c267bfae050af2a
czhhu <chengzhe.hu@polycom.com>
2018-08-26 13:22:41 +0800

    refactor printLinkedList add a concise impl for RemoveDuplicatedFromSortedListII82

--- src/main/java/my/czhhu/algo/common/CommonOp.java
+++ src/main/java/my/czhhu/algo/common/CommonOp.java
@@ -2,63 +2,78 @@ package my.czhhu.algo.common;
 
 import my.czhhu.algo.tree.VisitTreeByLevel;
 
-public class CommonOp {
+public class CommonOp
+{
 
-	public static Node findMidNode(Node head) {
-		if (head == null) {
-			return head;
-		}
+    public static Node findMidNode(Node head)
+    {
+        if (head == null)
+        {
+            return head;
+        }
 
-		Node slow;
-		Node fast = slow = head;
-		while (fast.getNext() != null && fast.getNext().getNext() != null) {
-			slow = fast.getNext();
-			fast = fast.getNext().getNext();
-		}
-		return slow;
-	}
+        Node slow;
+        Node fast = slow = head;
+        while (fast.getNext() != null && fast.getNext().getNext() != null)
+        {
+            slow = fast.getNext();
+            fast = fast.getNext().getNext();
+        }
+        return slow;
+    }
 
-	public static Node reverseLinkedList(Node head) {
-		if (head == null || head.getNext() == null)
-			return head;
-		return CommonOp.rld(head, head.getNext());
-	}
+    public static Node reverseLinkedList(Node head)
+    {
+        if (head == null || head.getNext() == null)
+            return head;
+        return CommonOp.rld(head, head.getNext());
+    }
 
-	public static void printLinkedList(Node head) {
-		if (head == null)
-			return;
-		while (head.getNext() != null) {
-			System.out.print(head.getData() + "->");
-			head = head.getNext();
-		}
-		System.out.println(head.getData());
-	}
+    public static void printLinkedList(Node head)
+    {
+        if (head == null)
+        {
+            System.out.println(head);
+            return;
+        }
+        while (head.getNext() != null)
+        {
+            System.out.print(head.getData() + "->");
+            head = head.getNext();
+        }
+        System.out.println(head.getData());
+    }
 
-	static private Node rld(Node cur, Node n) {
-		if (n.getNext() == null) {
-			n.setNext(cur);
-			return n;
-		}
-		Node tmp = n.getNext();
-		n.setNext(cur);
-		cur = n;
-		n = tmp;
-		return rld(cur, n);
+    static private Node rld(Node cur, Node n)
+    {
+        if (n.getNext() == null)
+        {
+            n.setNext(cur);
+            return n;
+        }
+        Node tmp = n.getNext();
+        n.setNext(cur);
+        cur = n;
+        n = tmp;
+        return rld(cur, n);
 
-	}
+    }
 
-	static public int length(Node n) {
-		if (n == null)
-			return -1;
-		int res = 0;
-		while (n != null) {
-			res++;
-			n = n.getNext();
-		}
-		return res;
-	}
+    static public int length(Node n)
+    {
+        if (n == null)
+            return -1;
+        int res = 0;
+        while (n != null)
+        {
+            res++;
+            n = n.getNext();
+        }
+        return res;
+    }
 
-	static public void printTreeByLevel(Tnode root) {
-		VisitTreeByLevel.visit(root, n -> System.out.print(n.data + " "), System.out::println);
-	}
+    static public void printTreeByLevel(Tnode root)
+    {
+        VisitTreeByLevel.visit(root, n -> System.out.print(n.data + " "), System.out::println);
+    }
 }
--- src/main/java/my/czhhu/algo/leetcode/RemoveDuplicatedFromSortedListII82.java
+++ src/main/java/my/czhhu/algo/leetcode/RemoveDuplicatedFromSortedListII82.java
@@ -6,6 +6,37 @@ import my.czhhu.algo.common.Node;
 public class RemoveDuplicatedFromSortedListII82
 {
 
+    static Node removeDupC(Node head)
+    {
+        Node dummy = new Node(-1);
+        dummy.next = head; // if head ==null || head.next ==null , return head here
+        Node tail = dummy;
+
+        Node n = head;
+        Node cur = head;
+
+        while (cur != null && cur.next != null)
+        {
+            while (cur.next != null && cur.data == cur.next.data)
+            {
+                cur = cur.next;
+
+            }
+
+            if (n == cur)
+            {
+                tail.next = cur;
+                tail = tail.next;
+            }
+
+            n = cur.next;
+            cur = n;
+
+        }
+        tail.next = n;
+        return dummy.next;
+    }
+
     static Node removeDup(Node head)
     {
         if (head == null || head.next == null)
@@ -23,9 +54,14 @@ public class RemoveDuplicatedFromSortedListII82
             {
                 cur = cur.next;
             }
-            if (cur == null) //in this case, last several elements are same, no need to add
+            if (cur == null)
+            {//in this case, last several elements are same, no need to add
+                if (found != null)
+                {
+                    found.next = null;
+                }
                 return h;
-
+            }
             if (n.next == cur) // n.data != cur.data, so cur is not moving
             {
                 if (found == null)
@@ -40,7 +76,6 @@ public class RemoveDuplicatedFromSortedListII82
                     found = n;
 
                 }
-                found.next = null;
 
             }
             n = cur;
@@ -51,7 +86,7 @@ public class RemoveDuplicatedFromSortedListII82
         {
             found.next = n; // this case is last.data != last-1.data, when n == last and cur ==null (last.next)
             found = n; //should append n to tail
-            found.next = null;
+            //found.next = null;
         }
 
         return h;
@@ -62,8 +97,13 @@ public class RemoveDuplicatedFromSortedListII82
     {
         Node head = new Node(1, new Node(2, new Node(3, new Node(3, new Node(4, new Node(4, new Node(5)))))));
         Node h1 = new Node(1, new Node(1, new Node(1, new Node(2, new Node(3, new Node(3))))));
+        Node h2 = new Node(1, new Node(1, new Node(2, new Node(2, new Node(3, new Node(3))))));
         CommonOp.printLinkedList(removeDup(head));
         CommonOp.printLinkedList(removeDup(h1));
+        System.out.println(removeDup(h2));
+        CommonOp.printLinkedList(removeDupC(head));
+        CommonOp.printLinkedList(removeDupC(h1));
+        System.out.println(removeDupC(h2));
 
     }
 
