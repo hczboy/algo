8b700a2d083ae9532318cb657e152589cafc1bac
arkie <arkie.hu@yahoo.com>
2018-09-03 00:53:32 +0800

    create topic MostRightTreeNode

---
+++ src/main/java/my/czhhu/algo/tree/MostRightTreeNode.java
@@ -0,0 +1,18 @@
+package my.czhhu.algo.tree;
+
+import java.util.List;
+
+import my.czhhu.algo.common.Tnode;
+
+public class MostRightTreeNode {
+
+	static List<Tnode> getMostRightTreeNode(Tnode root) {
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
