da3c173768079a515cc0562ae960bf922561f24f
arkie <arkie.hu@yahoo.com>
2020-04-05 23:13:58 +0800

    fix StringToInt overflow bug

--- src/main/java/my/czhhu/algo/classic/StringToInt.java
+++ src/main/java/my/czhhu/algo/classic/StringToInt.java
@@ -1,74 +1,58 @@
 package my.czhhu.algo.classic;
 
-public class StringToInt
-{
+public class StringToInt {
 
-    static int toInt(String s)
-    {
-        if (s == null || s.length() == 0)
-        {
-            throw new NumberFormatException("null or empty string");
-        }
-        int i = 0;
-        boolean isNegative = false;
-        String fs = s.trim();
-        while (fs.charAt(i) == '+' || fs.charAt(i) == '-')
-        {
-            if (fs.charAt(i) == '-')
-            {
-                isNegative = true;
-            }
-            i++;
+	static int toInt(String s) {
+		if (s == null || s.length() == 0) {
+			throw new NumberFormatException("null or empty string");
+		}
+		int i = 0;
+		boolean isNegative = false;
+		String fs = s.trim();
+		while (fs.charAt(i) == '+' || fs.charAt(i) == '-') {
+			if (fs.charAt(i) == '-') {
+				isNegative = true;
+			}
+			i++;
 
-        }
+		}
 
-        int start = i;
-        int res = 0;
-        char c;
-        int digit = 0;
-        while (start < fs.length())
-        {
-            c = fs.charAt(start++);
-            digit = c - '0';
-            if (digit < 0 || digit > 9)
-            {
-                throw new NumberFormatException(c + " is not number");
-            }
-            if (!isNegative)
-            {
-                if (res * 10 > Integer.MAX_VALUE - digit)
-                {
-                    throw new NumberFormatException("upper overflow");
-                }
-            }
-            else
-            {
-                if (res * 10 < Integer.MIN_VALUE + digit)
-                {
-                    throw new NumberFormatException("under overflow");
-                }
-            }
+		int start = i;
+		int res = 0;
+		char c;
+		int digit = 0;
+		while (start < fs.length()) {
+			c = fs.charAt(start++);
+			digit = c - '0';
+			if (digit < 0 || digit > 9) {
+				throw new NumberFormatException(c + " is not number");
+			}
+			if (!isNegative) {
+				if (res > Integer.MAX_VALUE / 10 || res == Integer.MAX_VALUE / 10 && digit > Integer.MAX_VALUE % 10) {
+					throw new NumberFormatException("upper overflow");
+				}
+			} else {
+				if (res < Integer.MIN_VALUE / 10 || res == Integer.MIN_VALUE / 10 && -digit < Integer.MIN_VALUE % 10) {
+					throw new NumberFormatException("under overflow");
+				}
+			}
 
-            if (isNegative)
-            {
-                res = res * 10 - digit;
-            }
-            else
-            {
-                res = res * 10 + digit;
-            }
+			if (isNegative) {
+				res = res * 10 - digit;
+			} else {
+				res = res * 10 + digit;
+			}
 
-        }
+		}
 
-        return res;
+		return res;
 
-    }
+	}
 
-    public static void main(String[] args)
-    {
-        System.out.println(toInt("2147483647"));
-        System.out.println(toInt("-2147483649"));
+	public static void main(String[] args) {
+		System.out.println(toInt("2147483647"));
+		System.out.println(toInt("-2147483648"));
 
-    }
+	}
 
 }
